46aed038119e167af4b33dc6c834950e455231c6
bytepack <miram13@protonmail.com>
2023-06-18 18:45:03 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- [Live Site URL](https://bytepack-frontendmentor-advice-generator.pages.dev/)
+
+### Built with
+
+- HTML5
+- CSS3
+- JS
---
+++ script.js
@@ -0,0 +1,50 @@
+const dice = document.querySelector("[data-dice]")
+const dicePhoto = document.querySelector("[data-photo-dice]")
+const advicePara = document.querySelector("[data-advice]")
+const adviceId = document.querySelector("[data-advice-id]")
+
+start()
+
+dice.addEventListener("click", () => {
+    start()
+})
+
+function start(){
+    renderAdviceBefore()
+    getAdvice().then(result => {
+        renderAdviceAfter(result)
+    })
+}
+
+async function getAdvice() {
+    const url = "https://api.adviceslip.com/advice"
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            console.log("Response wasn't ok")
+            return {msg: "API Error"}
+        }
+        const data = await response.json()
+        return data.slip || {msg: "Advice not found!"}
+    } catch (err) {
+        console.error("Yo, some errors happened, err.message:", err.message)
+        return {msg: "ERROR!"}
+    }
+
+}
+
+
+function renderAdviceAfter({advice, id, msg}) {
+    dicePhoto.classList.remove("advice__photo--animating")
+    if (advice == null) {
+        alert(msg)
+    } else {
+        advicePara.textContent = advice
+        adviceId.textContent = id.toString()
+    }
+
+}
+
+function renderAdviceBefore() {
+    dicePhoto.classList.add("advice__photo--animating")
+}
